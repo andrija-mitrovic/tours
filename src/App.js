6e6043c4920ca5920317a93e9da16163c604981f
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2022-01-22 17:29:18 +0100

    Add loading component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import './index.css';
+import { useState } from "react";
+import Loading from "./components/Loading";
+import "./index.css";
 
 function App() {
-  return (
-    <h2>Tours project aaaaaaa</h2>
-  );
+  const [loading, setLoading] = useState(true);
+
+  if (loading) {
+    return <Loading />;
+  }
+  return <h2>Tours project aaaaaaa</h2>;
 }
 
 export default App;
